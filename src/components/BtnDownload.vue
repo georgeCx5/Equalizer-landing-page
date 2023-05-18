3c4fac60dcd5f579c0e8ed934f7efcd562a14b25
georgeCx5 <69874793+georgeCx5@users.noreply.github.com>
2023-05-18 01:58:08 -0500

    Add files via upload

---
+++ src/components/BtnDownload.vue
@@ -0,0 +1,36 @@
+<script>
+import LogoiOS from '@/assets/gallery/starter-code/assets/icon-apple.svg?component'
+import LogoAndroid from '@/assets/gallery/starter-code/assets/icon-android.svg?component'
+
+export default {
+   components: {
+      LogoiOS,
+      LogoAndroid,
+   },
+   props: {
+      name: {
+         String,
+         default: 'No name',
+      },
+      bgColor: {
+         String,
+         default: 'bg-red-400',
+      },
+      bgHoverColor: {
+         String,
+         default: 'hover:bg-red-700',
+      },
+      txtColor: {
+         String,
+         default: 'text-neo-black',
+      }
+   }
+}
+</script>
+<template>
+   <button :class="`${bgColor} ${bgHoverColor} ${txtColor}`"
+      class=" w-[100%] h-[61px] flex justify-center items-center gap-2 text-[18px] leading-[34px] font-bold rounded-xl">
+      <component :is="`Logo${name}`" />
+      {{ name }} Download
+   </button>
+</template>
---
+++ src/components/BtnLink.vue
@@ -0,0 +1,21 @@
+<script>
+import IconFacebook from '@/assets/gallery/starter-code/assets/icon-facebook.svg?component'
+import IconInstagram from '@/assets/gallery/starter-code/assets/icon-instagram.svg?component'
+import IconTwitter from '@/assets/gallery/starter-code/assets/icon-twitter.svg?component'
+
+export default {
+   components: {
+      IconFacebook,
+      IconInstagram,
+      IconTwitter,
+   },
+   props: {
+      btnName: String,
+   }
+}
+</script>
+<template>
+   <button>
+      <component :is="`Icon${btnName}`" />
+   </button>
+</template>
